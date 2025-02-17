3b92b40dac66379b56228cdcbde6aeca88b6b753
bollettino.20159 <bollettino.andrea.studente@itispaleocapa.it>
2025-02-17 12:41:59 +0100

    Commit 1

--- src/Infisso.java
+++ src/Infisso.java
@@ -1,2 +1,16 @@
-public class Infisso {
+class Infisso {
+    private String materiale;
+    private double altezza;
+    private double larghezza;
+
+    public Infisso(String materiale, double altezza, double larghezza) {
+        this.materiale = materiale;
+        this.altezza = altezza;
+        this.larghezza = larghezza;
+    }
+
+    @Override
+    public String toString() {
+        return "Infisso (Materiale: " + materiale + ", Altezza: " + altezza + ", Larghezza: " + larghezza + ").";
+    }
 }
--- src/Mobile.java
+++ src/Mobile.java
@@ -1,2 +1,14 @@
-public class Mobile {
+class Mobile {
+    private double peso;
+    private double prezzo;
+
+    public Mobile(double peso, double prezzo) {
+        this.peso = peso;
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mobile [Peso: " + peso + " kg, Prezzo: " + prezzo + "€]";
+    }
 }
