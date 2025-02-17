a40a66ad48c8458b4758ddfa772266a47a3d7d35
bollettino.20159 <bollettino.andrea.studente@itispaleocapa.it>
2025-02-17 12:51:44 +0100

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Vettore<Infisso> vettoreInfissi = new Vettore<>();
+        vettoreInfissi.aggiungi(new Infisso("Legno", 2.0, 1.5));
+        vettoreInfissi.aggiungi(new Infisso("Alluminio", 2.2, 1.3));
 
-        System.out.printf("Hello and welcome!");
+        Vettore<Mobile> vettoreMobili = new Vettore<>();
+        vettoreMobili.aggiungi(new Mobile(30.5, 250.0));
+        vettoreMobili.aggiungi(new Mobile(15.0, 120.0));
 
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Lista Infissi:");
+        vettoreInfissi.visualizza();
 
-            System.out.println("i = " + i);
-
-        }
+        System.out.println("\nLista Mobili:");
+        vettoreMobili.visualizza();
     }
 }
--- src/Vettore.java
+++ src/Vettore.java
@@ -1,2 +1,19 @@
-public class Vettore {
+import java.util.ArrayList;
+
+class Vettore<T> {
+    private ArrayList<T> elementi;
+
+    public Vettore() {
+        elementi = new ArrayList<>();
+    }
+
+    public void aggiungi(T elemento) {
+        elementi.add(elemento);
+    }
+
+    public void visualizza() {
+        for (T elemento : elementi) {
+            System.out.println(elemento);
+        }
+    }
 }
